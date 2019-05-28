364a45908cd0d2339cad2f766d888915713f2726
Kami Xu <605464527@qq.com>
2019-05-28 16:16:49 +0800

    修复BUG

--- resume-3/message2.js
+++ resume-3/message2.js
@@ -1,28 +1,28 @@
 !function () {
-  var view = document.querySelector('section.message')
-
   var model = {
-    //初始化AV
-    initAV: function () {
-      var APP_ID = '7EA9ksK7jKboGlgHHxsasTIg-gzGzoHsz'
-      var APP_KEY = 'I7drp9Oq0cPvMSCAq1ON4aPp'
+    // 获取数据
+    init: function () {
+      var APP_ID = 'TsDnap9SEXjSvGSowP7gXXJC-gzGzoHsz'
+      var APP_KEY = 'rGye31p12mM3wFpNRn9RADu9'
       AV.init({ appId: APP_ID, appKey: APP_KEY })
     },
-    //获取数据
     fetch: function () {
       var query = new AV.Query('Message');
-      return query.find() //promise 对象
+      return query.find() // Promise 对象
     },
-    //创建数据
+    // 创建数据
     save: function (name, content) {
       var Message = AV.Object.extend('Message');
       var message = new Message();
-      return message.save({ // promise 对象
+      return message.save({  // Promise 对象
         'name': name,
         'content': content
       })
     }
   }
+
+  var view = document.querySelector('section.message')
+
 
   var controller = {
     view: null,
@@ -31,9 +31,10 @@
     init: function (view, model) {
       this.view = view
       this.model = model
+
       this.messageList = view.querySelector('#messageList')
       this.form = view.querySelector('form')
-      this.model.initAV()
+      this.model.init()
       this.loadMessages()
       this.bindEvents()
     },
@@ -44,21 +45,18 @@
           array.forEach((item) => {
             let li = document.createElement('li')
             li.innerText = `${item.name}: ${item.content}`
-            this.messageList.append(li)
+            this.messageList.appendChild(li)
           })
         }
       )
     },
     bindEvents: function () {
-      console.log(this)
-      this.form.addEventListener('submit',
-        (e) => {
-          console.log(this)
-          e.preventDefault()
-          this.saveMessages()
-        })
+      this.form.addEventListener('submit', function (e) {
+        e.preventDefault()
+        this.saveMessage()
+      })
     },
-    saveMessages: function () {
+    saveMessage: function () {
       let myForm = this.form
       let content = myForm.querySelector('input[name=content]').value
       let name = myForm.querySelector('input[name=name]').value
@@ -66,11 +64,15 @@
         let li = document.createElement('li')
         li.innerText = `${object.attributes.name}: ${object.attributes.content}`
         let messageList = document.querySelector('#messageList')
-        messageList.append(li)
+        messageList.appendChild(li)
         myForm.querySelector('input[name=content]').value = ''
-        myForm.querySelector('input[name=name]').value = ''
+        console.log(object)
       })
     }
+
   }
+
   controller.init(view, model)
+
+
 }.call()
