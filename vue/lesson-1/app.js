0a9478f4b6723c2c5edb19d386e62f268c9b0a5f
Kami Xu <605464527@qq.com>
2019-07-31 16:54:37 +0800

    缁戝畾浜嬩欢

--- vue/lesson-1/app.js
+++ vue/lesson-1/app.js
@@ -2,12 +2,19 @@ var app = new Vue({
   el:'#app',
   data:{
     name: "许爸爸",
+    age: "30",
     website: "http://www.baidu.com",
     websiteTag: "<a href='http://www.baidu.com'>百度</a>"
   },
   methods:{
     greet: function(time){
       return 'good' + ' ' + time + ' ' + this.name + '!'
+    },
+    add: function(inc){
+      this.age += inc;
+    },
+    sebtrate: function(dec){
+      this.age -= dec;
     }
   }
 })
