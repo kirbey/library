f4ee365e5eb433bcd310e9699fd7714560d87e5c
xkilla <605464527@qq.com>
2019-09-14 16:00:12 +0800

    1

--- js-resume/main.js
+++ js-resume/main.js
@@ -26,5 +26,33 @@ var id = setInterval(() => {
     styleTag.innerHTML = result.substring(0 , n)
     if(n >= result.length){
         window.clearInterval(id)
+        fn2()
+        fn3(result)
     }
-},100)
+},50)
+
+function fn2(){
+    var paper = document.createElement('div')
+    paper.id = 'paper'
+    document.body.appendChild(paper)
+}
+
+function fn3(preResult){
+    var result = `
+#paper{
+    background: red;
+    width: 100px;
+    height: 100px;
+}    
+    `
+    var n = 0
+    var id = setInterval(() => {
+        n += 1
+        code.innerHTML = preResult + result.substring(0 , n)
+        code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css)
+        styleTag.innerHTML = preResult + result.substring(0 , n)
+        if(n >= result.length){
+            window.clearInterval(id)
+        }
+    },50)
+}
