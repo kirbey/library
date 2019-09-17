14590bbc5ed3acb160bb115fb6b83bf8a166e823
xkilla <605464527@qq.com>
2019-09-17 21:58:17 +0800

    新加鼠标移动到图片显示GIF功能

---
+++ resume-3/js/lanrenzhijia.js
@@ -0,0 +1,44 @@
+﻿// 代码整理：懒人之家
+(function($){
+	$.fn.preview = function(){
+		var xOffset = 10;
+		var yOffset = 20;
+		var w = $(window).width();
+		$(this).each(function(){
+			$(this).hover(function(e){
+				if(/.png$|.gif$|.jpg$|.bmp$/.test($(this).attr("href"))){
+					$("body").append("<div id='preview'><div><img src='"+$(this).attr('href')+"' /><p>"+$(this).attr('title')+"</p></div></div>");
+				}else{
+					$("body").append("<div id='preview'><div><p>"+$(this).attr('title')+"</p></div></div>");
+				}
+				$("#preview").css({
+					position:"absolute",
+					padding:"2px",
+					backgroundColor:"#fff",
+					top:(e.pageY - yOffset) + "px",
+					zIndex:1000
+				});
+				$("#preview > div").css({
+					padding:"3px"
+				});
+				if(e.pageX < w/2){
+					$("#preview").css({
+						left: e.pageX + xOffset + "px",
+						right: "auto"
+					}).fadeIn("fast");
+				}else{
+					$("#preview").css("right",(w - e.pageX + yOffset) + "px").css("left", "auto").fadeIn("fast");	
+				}
+			},function(){
+				$("#preview").remove();
+			}).mousemove(function(e){
+				$("#preview").css("top",(e.pageY - xOffset) + "px")
+				if(e.pageX < w/2){
+					$("#preview").css("left",(e.pageX + yOffset) + "px").css("right","auto");
+				}else{
+					$("#preview").css("right",(w - e.pageX + yOffset) + "px").css("left","auto");
+				}
+			});						  
+		});
+	};
+})(jQuery);
