2667057a91c30aa2442b4c16a016072228d78ad8
xkilla <605464527@qq.com>
2019-10-06 15:47:45 +0800

    加条件判断

--- nodejs/todo/app.js
+++ nodejs/todo/app.js
@@ -1,14 +1,31 @@
 var fs = require('fs')
-
-const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
-const list = JSON.parse(fileContent);
-
+var path = require('path')
 const verb = process.argv[2]
 const content = process.argv[3]
 
-const task = content
-list.push(task)
+if(verb === 'add'){
+    fs.stat('D:\\library\\nodejs\\todo\\db', function(err, stat){
+        if(err === null){
+            const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
+            const list = JSON.parse(fileContent);
+            const task = content
+            list.push([task, false])
+            fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
+            console.log(list)
+        }else if(err.code === 'ENOENT'){
+            fs.writeFileSync('D:\\library\\nodejs\\todo\\db', '')
+            const list = []
+            const task = content
+            list.push([task, false])
+            fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
+            console.log(list)
+        }else{
+            console.log('Something other error: ',err.code);
+        }
+    });
+}else if(verb === 'list'){
+    const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
+    const list = JSON.parse(fileContent);
+    console.log(list)
+}
 
-fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
-
-console.log(list)
