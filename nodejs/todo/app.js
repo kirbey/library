3e999f578c4615372e2194b77c6bdd82e7d00856
xkilla <605464527@qq.com>
2019-10-06 16:05:29 +0800

    添加完成和编写

--- nodejs/todo/app.js
+++ nodejs/todo/app.js
@@ -2,6 +2,7 @@ var fs = require('fs')
 var path = require('path')
 const verb = process.argv[2]
 const content = process.argv[3]
+const content2 = process.argv[4]
 
 if(verb === 'add'){
     fs.stat('D:\\library\\nodejs\\todo\\db', function(err, stat){
@@ -26,6 +27,27 @@ if(verb === 'add'){
 }else if(verb === 'list'){
     const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
     const list = JSON.parse(fileContent);
+    console.log(list)
+}else if(verb === 'delete'){
+    const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
+    const list = JSON.parse(fileContent);
+    const n = content
+    list.splice(n - 1, 1)
+    fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
+    console.log(list)
+}else if(verb === 'done'){
+    const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
+    const list = JSON.parse(fileContent);
+    const n = content
+    list[n-1][1] = true
+    fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
+    console.log(list)
+}else if(verb === 'edit'){
+    const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
+    const list = JSON.parse(fileContent);
+    const n = content
+    list[n-1][0] = content2
+    fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
     console.log(list)
 }
 
