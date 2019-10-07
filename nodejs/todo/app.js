f7f8bac504fd46d1ad550946d462187f5d7619aa
xkilla <605464527@qq.com>
2019-10-07 21:33:45 +0800

    代码优化

--- nodejs/todo/app.js
+++ nodejs/todo/app.js
@@ -3,51 +3,57 @@ var path = require('path')
 const verb = process.argv[2]
 const content = process.argv[3]
 const content2 = process.argv[4]
+const dbPath = 'D:\\library\\nodejs\\todo\\db'
 
-if(verb === 'add'){
-    fs.stat('D:\\library\\nodejs\\todo\\db', function(err, stat){
-        if(err === null){
-            const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
-            const list = JSON.parse(fileContent);
+function save (list){
+    fs.writeFileSync(dbPath, JSON.stringify(list))
+}
+
+function fetch (){
+    const fileContent = fs.readFileSync(dbPath).toString()
+    const list = JSON.parse(fileContent);
+    return list
+}
+
+if (verb === 'add') {
+    fs.stat(dbPath, function (err, stat) {
+        if (err === null) {
+            const list = fetch()
             const task = content
             list.push([task, false])
-            fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
+            save(list)
             console.log(list)
-        }else if(err.code === 'ENOENT'){
-            fs.writeFileSync('D:\\library\\nodejs\\todo\\db', '')
+        } else if (err.code === 'ENOENT') {
+            fs.writeFileSync(dbPath, '')
             const list = []
             const task = content
             list.push([task, false])
-            fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
+            save(list)
             console.log(list)
-        }else{
-            console.log('Something other error: ',err.code);
+        } else {
+            console.log('Something other error: ', err.code);
         }
     });
-}else if(verb === 'list'){
-    const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
-    const list = JSON.parse(fileContent);
+} else if (verb === 'list') {
+    const list = fetch()
     console.log(list)
-}else if(verb === 'delete'){
-    const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
-    const list = JSON.parse(fileContent);
+} else if (verb === 'delete') {
+    const list = fetch()
     const n = content
     list.splice(n - 1, 1)
-    fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
+    save(list)
     console.log(list)
-}else if(verb === 'done'){
-    const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
-    const list = JSON.parse(fileContent);
+} else if (verb === 'done') {
+    const list = fetch()
     const n = content
-    list[n-1][1] = true
-    fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
+    list[n - 1][1] = true
+    save(list)
     console.log(list)
-}else if(verb === 'edit'){
-    const fileContent = fs.readFileSync('D:\\library\\nodejs\\todo\\db').toString()
-    const list = JSON.parse(fileContent);
+} else if (verb === 'edit') {
+    const list = fetch()
     const n = content
-    list[n-1][0] = content2
-    fs.writeFileSync('D:\\library\\nodejs\\todo\\db', JSON.stringify(list))
+    list[n - 1][0] = content2
+    save(list)
     console.log(list)
 }
 
